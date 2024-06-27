51f1ea5cc3d468520b6ef46a458a0d904640d6a3
Rajyalakshmi <V.rajyalakshmi114@gmail.com>
2024-06-27 16:48:26 +0530

    DAY2

---
+++ DAY2/int_float.py
@@ -0,0 +1,14 @@
+num1 =5.4
+num2 =9.7
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print(add)
+print(sub)
+print(mul)
+print(div)
+round = round(div,2)
+print(round)
+abs = abs(sub)
+print(abs)
---
+++ DAY2/strings.py
@@ -0,0 +1,38 @@
+
+str1 = "HELLO"
+str2 = "world"
+str3 = "sky is blue"
+
+# string concat
+concat = str1 + str2 
+print (concat)
+
+# string length
+Length = len(str3)
+print (Length)
+
+# string type
+type = type (str1)
+print (type)
+
+#string replace
+replace = str3.replace("sky","clouds")
+print (replace)
+
+#string upper and lower
+Upper = str2.upper()
+lower = str1.lower()
+print(Upper)
+print(lower)
+
+#string split
+split = str3.split()
+print(split)
+
+#string strip
+str4 = "     rajyalakshmi     "
+strip =str4.strip()
+print (str4)
+print(strip)
+
+
