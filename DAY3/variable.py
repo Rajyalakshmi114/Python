e287293da8fbb4aa8f0889c86cab9877ccbbfd6f
Rajyalakshmi <V.rajyalakshmi114@gmail.com>
2024-06-28 16:59:45 +0530

    DAY3

---
+++ DAY3/variable.py
@@ -0,0 +1,29 @@
+# assigning the value of variable
+my_variable = 69
+
+# acessing the value to variable
+print(my_variable)
+
+# Local variable
+
+def my_function():
+
+    x = "raji"
+    print(x)
+my_function()    
+
+ # global variable
+y = 96
+def janu():
+    print(y)
+    
+janu() 
+
+
+
+
+
+
+
+    
+
