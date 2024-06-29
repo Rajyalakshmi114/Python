8fd798bb4cb6722cd733818388b9165abf533b9f
Rajyalakshmi <V.rajyalakshmi114@gmail.com>
2024-06-29 17:08:25 +0530

    DAY4

--- DAY3/variable.py
+++ DAY3/variable.py
@@ -32,6 +32,19 @@ def sub():
 addition()
 sub()      
 
+# example for local variable
+
+def addition():
+    a=10
+    b=5
+    print(a+b)
+
+def sub():
+    print(a - b)
+
+addition()
+sub()      
+
 
 
 
