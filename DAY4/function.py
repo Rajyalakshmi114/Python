ab336ce8de78379d1ceeaa184bd8adc9632da6ff
Rajyalakshmi <V.rajyalakshmi114@gmail.com>
2024-07-01 14:23:26 +0530

    DAY4

--- DAY4/function.py
+++ DAY4/function.py
@@ -1,4 +1,19 @@
-def test(a, b):
-    add = a + b
-    print(add)
-test(4,5)
+num1 = 10
+num2 = 5
+ 
+def addition():
+     add = num1 + num2
+     print(add)
+
+
+def sub():
+    sub = num1 - num2
+    print(sub)
+
+def mul():
+    mul = num1 * num2
+    print(mul)
+
+addition()
+sub()
+mul()   
