8fe206c2dab47a24b109ce33ae0d93143f823cac
Rajyalakshmi <V.rajyalakshmi114@gmail.com>
2024-07-03 12:10:51 +0530

    Assignment

---
+++ DAY6/comparison.py
@@ -0,0 +1,13 @@
+a = 11
+b = 9
+
+less_than = a < b
+greater_than = a > b
+less_than_or_equal = a <= b
+greater_than_or_equal = a >= b
+
+
+print("a < b:", less_than)
+print("a > b:", greater_than)
+print("a <= b:", less_than_or_equal)
+print("a >= b:", greater_than_or_equal)
