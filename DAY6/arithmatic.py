ef75f5ace06d8712e7fd5bad97de98c70a3a5133
Rajyalakshmi <V.rajyalakshmi114@gmail.com>
2024-07-03 11:57:54 +0530

    DAY6

---
+++ DAY6/arithmatic.py
@@ -0,0 +1,13 @@
+a = 11
+b =9
+
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+quotient_result = a / b
+
+
+print("sum:", sum_result)
+print("difference:", difference_result)
+print("product:", product_result)
+print("quotient:", quotient_result)
