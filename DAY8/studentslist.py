a7fbc505e42a863f3a707cd5a4a14db11431c308
Rajyalakshmi <V.rajyalakshmi114@gmail.com>
2024-07-09 15:07:18 +0530

    DAY8

--- DAY8/studentslist.py
+++ DAY8/studentslist.py
@@ -2,3 +2,8 @@
 
 students_names = ["raji","siri","venu","raja","tirmula" ]
 print(students_names)
+
+#bucket list
+
+s3_buckets = ["raji_demo_bucket","siri_demo_bucket","venu_demo_bucket","raja_demo_bucket","tirmula_demo_bucket" ]
+print(s3_buckets)
