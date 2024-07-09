62bb32f6b3994fc67af855fd3ea3e04529ccb28a
Rajyalakshmi <V.rajyalakshmi114@gmail.com>
2024-07-09 15:48:33 +0530

    DAY8

--- DAY8/studentslist.py
+++ DAY8/studentslist.py
@@ -7,3 +7,25 @@ print(students_names)
 
 s3_buckets = ["raji_demo_bucket","siri_demo_bucket","venu_demo_bucket","raja_demo_bucket","tirmula_demo_bucket" ]
 print(s3_buckets)
+
+#adding new name to lists
+
+s3_buckets_lists = ["raji_demo_bucket","siri_demo_bucket","venu_demo_bucket","raja_demo_bucket","tirmula_demo_bucket" ]
+s3_buckets_lists.append("pinky_demo_bucket") 
+print(s3_buckets_lists)
+
+#remove name from the lists
+
+s3_buckets_lists = ["raji_demo_bucket","siri_demo_bucket","venu_demo_bucket","raja_demo_bucket","tirmula_demo_bucket", "pinky_demo_bucket" ]
+s3_buckets_lists.remove("pinky_demo_bucket") 
+print(s3_buckets_lists)
+
+#tuple
+
+s3_buckets = ("raji_demo_bucket","siri_demo_bucket","venu_demo_bucket","raja_demo_bucket","tirmula_demo_bucket" )
+print(s3_buckets)
+
+# type is used to identify the class
+
+s3_buckets = ("raji_demo_bucket","siri_demo_bucket","venu_demo_bucket","raja_demo_bucket","tirmula_demo_bucket" )
+print(tpye(s3_buckets))
